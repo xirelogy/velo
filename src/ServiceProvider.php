eea563eaa0a4b3a407f81ce743d1227a7934f1e4
xirelogy <jx@tou.st>
2021-02-12 19:52:36 +0800

    Compatibility with Laravel 7.x

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -4,11 +4,32 @@ namespace Xirelogy\Velo;
 
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use Illuminate\Support\Str;
 use Illuminate\View\Compilers\BladeCompiler;
+use SplFileInfo;
+use Symfony\Component\Finder\Finder;
 
 class ServiceProvider extends BaseServiceProvider
 {
+    /**
+     * The path where view resources are located
+     */
     private const VIEW_PATH = __DIR__ . '/../resources/views';
+
+    /**
+     * The path where manual components are located
+     */
+    private const COMPONENTS_PATH = __DIR__ . '/../src/View/Components';
+
+    /**
+     * The namespace where components resides
+     */
+    private const COMPONENT_NAMESPACE = 'Xirelogy\\Velo\\View\\Components';
+
+    /**
+     * The namespace prefix when component is used
+     */
+    private const COMPONENT_PREFIX = 'velo';
 
 
     /**
@@ -16,9 +37,51 @@ class ServiceProvider extends BaseServiceProvider
      */
     public function boot() : void
     {
-        $this->loadViewsFrom(self::VIEW_PATH, 'velo');
+        $this->loadViewsFrom(self::VIEW_PATH, self::COMPONENT_PREFIX);
 
-        Blade::componentNamespace('Xirelogy\\Velo\\View\\Components', 'velo');
+        $this->bootManualComponents();
+    }
+
+
+    /**
+     * Bootstrap the manual components
+     */
+    private function bootManualComponents() : void
+    {
+        $componentsPath = realpath(self::COMPONENTS_PATH);
+        if (!Str::endsWith($componentsPath, '/')) $componentsPath .= '/';
+
+        $files = (new Finder())->files()->name('*.php')->in([$componentsPath]);
+
+        collect($files)->each(function (SplFileInfo $fileInfo) use($componentsPath) {
+            self::bootManualComponentFrom($componentsPath, $fileInfo);
+        });
+    }
+
+
+    /**
+     * Bootstrap specific manual component
+     * @param string $componentsPath
+     * @param SplFileInfo $fileInfo
+     */
+    private static function bootManualComponentFrom(string $componentsPath, SplFileInfo $fileInfo) : void
+    {
+        $currentPath = $fileInfo->getRealPath();
+        if (!Str::startsWith($currentPath, $componentsPath)) return;
+
+        $relPath = substr($currentPath, strlen($componentsPath));
+        if (Str::endsWith($relPath, '.php')) $relPath = substr($relPath, 0, strlen($relPath) - 4);
+
+        $componentName = '';
+        $componentNamespace = self::COMPONENT_NAMESPACE;
+        $relPathSegments = explode('/', $relPath);
+        foreach ($relPathSegments as $relPathSegment) {
+            $componentName .= '.' . Str::kebab($relPathSegment);
+            $componentNamespace .= '\\' . $relPathSegment;
+        }
+        $componentName = substr($componentName, 1);
+
+        Blade::component($componentNamespace, self::COMPONENT_PREFIX . '::' . $componentName);
     }
 
 
