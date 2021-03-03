01a5de9fd23ba956ecf61a4d0a58eddef7c5d49b
xirelogy <jx@tou.st>
2021-03-03 16:55:39 +0800

    Add pushonce/endpushonce blade directive

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -37,9 +37,28 @@ class ServiceProvider extends BaseServiceProvider
      */
     public function boot() : void
     {
+        $this->addBladeDirectives();
+
         $this->loadViewsFrom(self::VIEW_PATH, self::COMPONENT_PREFIX);
 
         $this->bootManualComponents();
+    }
+
+
+    /**
+     * Add Blade directives
+     */
+    private function addBladeDirectives() : void
+    {
+        Blade::directive('pushonce', function($expression) {
+            [$pushName, $pushSub] = explode(':', trim(substr($expression, 1, -1)));
+            $key = '__pushonce_'.str_replace('-', '_', $pushName).'_'.str_replace('-', '_', $pushSub);
+            return "<?php if(! isset(\$__env->{$key})): \$__env->{$key} = 1; \$__env->startPush('{$pushName}'); ?>";
+        });
+
+        Blade::directive('endpushonce', function() {
+            return '<?php $__env->stopPush(); endif; ?>';
+        });
     }
 
 
